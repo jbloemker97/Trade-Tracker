d3f40d0097716054fced0218c114704f175622bb
Jacob Bloemker <jbloemker97@gmail.com>
2018-09-09 13:29:07 -0400

    Worked with Navbar adding login/logout functionality

--- trades/models.py
+++ trades/models.py
@@ -9,8 +9,8 @@ class Trades(models.Model):
     ticker = models.CharField(max_length=5)
     position = models.CharField(max_length=100, choices=CHOICES, default='Long')
     shares = models.PositiveIntegerField(default=0)
-    entry_date = models.DateTimeField(timezone.now())
-    exit_date = models.DateTimeField(timezone.now(), blank=True, null=True)
+    entry_date = models.DateTimeField(timezone.now()) # xx/xx/xx
+    exit_date = models.DateTimeField(timezone.now(), blank=True, null=True) # xx/xx/xx
     entry_price = models.DecimalField(max_digits=6, decimal_places=2)
     exit_price = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
     pnl = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 app_name = 'user'
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('account/', views.account, name="my_account"),
+    path('account/', views.account, name='my_account'),
+    path('login/', TemplateView.as_view(template_name='registration/login.html'), name='login'),
 ]
