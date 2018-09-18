44e5bb30f1c4a8eadeaf6665da667f821ef7f699
Jacob Bloemker <jbloemker97@gmail.com>
2018-09-18 19:35:30 -0400

    Visualizing Data

--- charts/static/charts/js/getData.js
+++ charts/static/charts/js/getData.js
@@ -1,51 +1,36 @@
-function getData (callback) {
-    $.ajax({
-        url: "/charts/data",
-        method: "GET",
-        success: function (res) {
-            callback(res);
-        }
-    });
-}
 
-var ctx = document.getElementById("chart1").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '',
-            //data: [12, 19, 3, 5, 2, 3],
-            data: getData((res) => {
-                console.log(res);
-                return res;
-            }),
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
+$.ajax({
+    url: "/charts/data",
+    method: "GET",
+    success: function (res) {
+        let finalizedData = [];
+        res.forEach(el => {
+            finalizedData.push(parseInt(el.pnl));
+        });
+    
+        var ctx = document.getElementById("chart1").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ["Red", "Blue", "Yellow"],
+                datasets: [{
+                    label: 'PNL',
+                    data: finalizedData,
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
                 }
-            }]
-        }
+            }
+        });
+    },
+    error: function (err) {
+        console.log(err)
     }
 });
+
