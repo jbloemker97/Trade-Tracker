18cddc7831931ccdd61153f4fd06b3fa653fc816
Jacob Bloemker <jbloemker97@gmail.com>
2018-09-30 11:42:22 -0400

    Adding user profile if doesn't exist. Updating if does

--- user/views.py
+++ user/views.py
@@ -24,24 +24,29 @@ def account(request):
     if request.method == 'POST':
         form = UpdateProfileForm(request.POST)
 
-        
         full_name = request.POST.get('full_name')
         profile_image = request.POST.get('profile_image')
         account_balance = request.POST.get('account_balance')
         starting_balance = request.POST.get('starting_balance')
 
-        print(full_name)
-        print(profile_image)
-        print(account_balance)
-        print(starting_balance)
-
-        Profile.objects.create(
+        profile, created = Profile.objects.get_or_create(
             user=request.user,
-            full_name=full_name,
-            profile_image=profile_image,
-            account_balance=account_balance,
-            starting_balance=starting_balance
-        ).save()
+            defaults={
+                'full_name': full_name,
+                'profile_image': profile_image,
+                'account_balance': account_balance,
+                'starting_balance': starting_balance
+            }
+        )
+
+        if not created:
+            Profile.objects.filter(user_id=request.user.id).update(
+                user=request.user,
+                full_name=full_name,
+                profile_image=profile_image,
+                account_balance=account_balance,
+                starting_balance=starting_balance
+            )
 
         return HttpResponseRedirect(reverse('user:my_account'))
     else:
